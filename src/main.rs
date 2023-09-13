d7f802faf528e9c306a23cf72d626e96e82148cd
h4rl <h4rldev@proton.me>
2023-09-13 22:34:20 +0200

    just try to implement this, why are there no docs for this shit.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
 use anyhow::Context as _;
-use poise::serenity_prelude as serenity;
-use shuttle_secrets::SecretStore;
+use poise::{serenity_prelude as serenity, PartialContext};
 use shuttle_poise::ShuttlePoise;
+use shuttle_secrets::SecretStore;
+use std::sync::Mutex;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+struct Data {
+    prefix: Mutex<Option<String>>,
+} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
+async fn dynamic_prefix(
+    ctx: poise::PartialContext<'_, Data, Error>,
+) -> Result<Option<String>, Error> {
+    // Here you can fetch the prefix dynamically, for example from a database.
+    // For simplicity, we return a static prefix here.
+    Ok(Some("my_prefix".to_string()))
+}
+
 /// Responds with "world!"
-#[poise::command(slash_command)]
+#[poise::command(slash_command, prefix_command)]
 async fn hello(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("world!").await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+async fn wood(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("world!").await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+async fn prefix(ctx: Context<'_>, prefix: String) -> Result<(), Error> {
+    let mut data = ctx.data().prefix.lock().expect("Can't lock!?");
+    *data = Some(prefix);
     Ok(())
 }
 
@@ -23,7 +47,17 @@ async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> Shuttle
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![hello()],
+            commands: vec![hello(), wood(), prefix()],
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: None,
+                dynamic_prefix: Some(Box::pin(async move { Ok(Some("my_prefix".to_string())) })),
+                edit_tracker: Some(poise::EditTracker::for_timespan(
+                    std::time::Duration::from_secs(3600),
+                )),
+                case_insensitive_commands: true,
+                mention_as_prefix: true,
+                ..Default::default()
+            },
             ..Default::default()
         })
         .token(discord_token)
@@ -31,7 +65,7 @@ async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> Shuttle
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data { prefix })
             })
         })
         .build()
